f5f0af8f52402d31c6325e38ccd096d0e1025687
Yaroslav <69323968+eReader1@users.noreply.github.com>
2021-03-23 18:41:24 +0200

    asd

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.filter__item').click(function(event) {
+		var i==$(this).data('filter');
+	if (i==1) {
+		$('.portfolio__column').show();
+	}else{
+		$('.portfolio__column').hide();
+		$('.portfolio__column.f_'+i).show();
+	}
+
+	return false
+});
